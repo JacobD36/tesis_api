025fdc1a388eb1570ccd341f8aed8fae4888e016
Jaime Pérez Frias <proveedor327@culqi.com>
2023-10-28 12:44:28 -0500

    fix: se corrige url de db

--- database/config.ts
+++ database/config.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export const dbConnection = async() => {
     try {
-        const url = process.env.MONGODB_CNN || '';
+        //const url = process.env.MONGODB_CNN || '';
+        const url = 'mongodb+srv://user_node_cafe:kbjnfqfsfy79@cluster0.j4nbw.mongodb.net/tesis?retryWrites=true&w=majority';
         await mongoose.connect(url ,{
             serverSelectionTimeoutMS: 5000,
             family: 4,
